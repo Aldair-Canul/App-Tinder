1db2c97b20000aed58a245006458d3be6896b7ad
LeonardoGongora10 <gongoraleonardo34@gmail.com>
2026-09-23 12:55:57 -0600

    :sparkles: feat: Cambio en la solicitud de Nombre a Correo

--- Tinder2/App-Tinder2.0/src/main/java/Controladores/ControladorRegistro.java
+++ Tinder2/App-Tinder2.0/src/main/java/Controladores/ControladorRegistro.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.Period;
 
 public class ControladorRegistro {
-    
+
     @FXML private TextField txtNombre;
     @FXML private TextField txtApellidos;
     @FXML private TextField txtCorreo;
